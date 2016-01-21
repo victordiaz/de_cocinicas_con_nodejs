a493276ca771c377e43508e7f1ed30324cfa0380
AngelLM <alm37454@gmail.com>
2016-01-21 11:00:02 +0100

    mejorado

--- 05_reto_1/main.js
+++ 05_reto_1/main.js
@@ -1,16 +1,9 @@
-function processFile(inputFile) {
-    var fs = require('fs'),
-        readline = require('readline'),
-        instream = fs.createReadStream(inputFile),
-        outstream = new (require('stream'))(),
-        rl = readline.createInterface(instream, outstream);
-     
-    rl.on('line', function (line) {
-        console.log(line+ " me mola.");
-    });
-    
-    rl.on('close', function (line) {
-        console.log(line);
-    });
-}
-processFile('comida.txt');
+var fs = require('fs'),
+    readline = require('readline'),
+    instream = fs.createReadStream('comida.txt'),
+    outstream = new (require('stream'))(),
+    rl = readline.createInterface(instream, outstream);
+
+rl.on('line', function (line) {
+    console.log(line+ " me mola.");
+});
