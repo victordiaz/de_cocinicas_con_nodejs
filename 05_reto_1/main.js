b24c74bda8b0010c8ba05dcf6ba5e2b632be1dec
AngelLM <alm37454@gmail.com>
2016-01-21 10:49:58 +0100

    Reto 1 resuelto

--- 05_reto_1/main.js
+++ 05_reto_1/main.js
@@ -1,1 +1,16 @@
-fs = require('fs');
+function processFile(inputFile) {
+    var fs = require('fs'),
+        readline = require('readline'),
+        instream = fs.createReadStream(inputFile),
+        outstream = new (require('stream'))(),
+        rl = readline.createInterface(instream, outstream);
+     
+    rl.on('line', function (line) {
+        console.log(line+ " me mola.");
+    });
+    
+    rl.on('close', function (line) {
+        console.log(line);
+    });
+}
+processFile('comida.txt');
